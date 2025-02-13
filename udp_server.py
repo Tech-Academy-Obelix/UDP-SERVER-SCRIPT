a73fa42e23ff722548af38974c23c473d7de9b71
ZDRAVKO107UKTC <146438118+ZDRAVKO107UKTC@users.noreply.github.com>
2025-02-13 15:50:20 +0200

    Add files via upload

--- udp_server.py
+++ udp_server.py
@@ -1,7 +1,10 @@
 import socket
+import subprocess
+
 
 HOST = "0.0.0.0"
 PORT = 12345
+script = "/home/zdravko/PycharmProjects/UDP-SERVER-SCRIPT/restart.sh"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((HOST, PORT))
@@ -11,3 +14,19 @@ print(f"UDP сървър стартиран на порт {PORT}")
 while True:
     data, addr = sock.recvfrom(1024)
     print(f"Получено от {addr}: {data.decode()}")
+
+    try:
+        process = subprocess.Popen(
+            [script], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+        )
+
+        for line in process.stdout:
+            print(line, end="")
+
+        process.wait()
+
+        if process.returncode != 0:
+            print(f"Грешка при изпълнение на скрипта: {process.stderr.read()}")
+
+    except Exception as e:
+        print(f"Грешка: {e}")
